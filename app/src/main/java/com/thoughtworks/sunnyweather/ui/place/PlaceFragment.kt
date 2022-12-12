69a71b1691e207849bea983a4cbed17744fe3914
Eason-T <awawerwer2007@163.com>
2022-12-12 16:46:25 +0800

    refactor:place fragment logic to use binding

--- app/src/main/java/com/thoughtworks/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/thoughtworks/sunnyweather/ui/place/PlaceFragment.kt
@@ -18,8 +18,6 @@ import com.thoughtworks.sunnyweather.databinding.FragmentPlaceBinding
 
 class PlaceFragment : Fragment() {
     val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
-
-    private var visible: Boolean = false
     private var _binding: FragmentPlaceBinding? = null
     private val binding get() = _binding!!
     private lateinit var adapter: PlaceAdapter
@@ -28,73 +26,47 @@ class PlaceFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPlaceBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    @Deprecated("Deprecated in Java")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val bgImgView = view?.findViewById<ImageView>(R.id.bgImageView)
-        val recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerView)
-        val searchPlaceEdit = view?.findViewById<EditText>(R.id.searchPlaceEdit)
-
+        val adapter = PlaceAdapter(this, viewModel.placeList)
         val layoutManager = LinearLayoutManager(activity)
-        recyclerView?.layoutManager = layoutManager
-        adapter = PlaceAdapter(this, viewModel.placeList)
-        recyclerView?.adapter = adapter
 
-        searchPlaceEdit?.addTextChangedListener { editable ->
-            val content = editable.toString()
-            if (content.isNotEmpty()) {
-                viewModel.searchPlaces(content)
-            } else {
-                recyclerView?.visibility = View.GONE
-                bgImgView?.visibility = View.VISIBLE
-                viewModel.placeList.clear()
-                adapter.notifyDataSetChanged()
+        binding.apply {
+            recyclerView.layoutManager = layoutManager
+            recyclerView.adapter = adapter
+
+            searchPlaceEdit.addTextChangedListener { editable ->
+                val content = editable.toString()
+                if (content.isNotEmpty()) {
+                    viewModel.searchPlaces(content)
+                } else {
+                    recyclerView.visibility = View.GONE
+                    bgImageView.visibility = View.VISIBLE
+                    viewModel.placeList.clear()
+                    adapter.notifyDataSetChanged()
+                }
+            }
+            viewModel.placeLiveData.observe(viewLifecycleOwner) { result ->
+                val place = result.getOrNull()
+                if (place != null) {
+                    recyclerView.visibility = View.VISIBLE
+                    bgImageView.visibility = View.GONE
+                    viewModel.placeList.clear()
+                    viewModel.placeList.addAll(place)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_LONG).show()
+                    result.exceptionOrNull()?.printStackTrace()
+                }
             }
         }
 
-        viewModel.placeLiveData.observe(viewLifecycleOwner) { result ->
-            val place = result.getOrNull()
-            if (place != null) {
-                recyclerView?.visibility = View.VISIBLE
-                bgImgView?.visibility = View.GONE
-                viewModel.placeList.clear()
-                viewModel.placeList.addAll(place)
-                adapter.notifyDataSetChanged()
-            } else {
-                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_LONG).show()
-                result.exceptionOrNull()?.printStackTrace()
-            }
-        }
-    }
-
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private const val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private const val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private const val UI_ANIMATION_DELAY = 300
     }
 
     override fun onDestroyView() {
